909da56b425a6b90da8e77f6f174977afa9004be
acornspeac-create <acornspeac@gmail.com>
2026-09-22 23:27:54 +0500

    Add KOT maximum access framework

---
+++ app/src/main/java/tj/kod/assistant/KotAccessibilityService.kt
@@ -0,0 +1,10 @@
+package tj.kod.assistant
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class KotAccessibilityService : AccessibilityService() {
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) = Unit
+
+    override fun onInterrupt() = Unit
+}
