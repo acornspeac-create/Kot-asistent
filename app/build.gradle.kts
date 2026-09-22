038e6c69ea1bd0df50b11a2cf1187486329ae27d
acornspeac-create <acornspeac@gmail.com>
2026-09-23 00:35:19 +0500

    Add Shizuku API support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation("androidx.work:work-runtime-ktx:2.10.0")
+    implementation("dev.rikka.shizuku:api:13.1.5")
+    implementation("dev.rikka.shizuku:provider:13.1.5")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
