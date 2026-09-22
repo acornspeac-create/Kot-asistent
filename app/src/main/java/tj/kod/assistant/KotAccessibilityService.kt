7565e415b2669842f7a3c0ba1eab4db98fc086c8
acornspeac-create <acornspeac@gmail.com>
2026-09-22 23:43:53 +0500

    Expose active KOT accessibility service

--- app/src/main/java/tj/kod/assistant/KotAccessibilityService.kt
+++ app/src/main/java/tj/kod/assistant/KotAccessibilityService.kt
@@ -4,7 +4,25 @@ import android.accessibilityservice.AccessibilityService
 import android.view.accessibility.AccessibilityEvent
 
 class KotAccessibilityService : AccessibilityService() {
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        instance = this
+    }
+
     override fun onAccessibilityEvent(event: AccessibilityEvent?) = Unit
 
     override fun onInterrupt() = Unit
+
+    override fun onDestroy() {
+        if (instance === this) {
+            instance = null
+        }
+        super.onDestroy()
+    }
+
+    companion object {
+        @Volatile
+        var instance: KotAccessibilityService? = null
+            private set
+    }
 }
