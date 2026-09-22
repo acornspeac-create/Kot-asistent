159d22e757f08d9f316ccbaa701ce1575508180c
acornspeac-create <acornspeac@gmail.com>
2026-09-22 21:21:57 +0500

    Pin development signing configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,23 @@ android {
         versionName = "0.1." + ciRunNumber
     }
 
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file(
+                System.getProperty("user.home") + "/.android/debug.keystore"
+            )
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
+    buildTypes {
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
     buildFeatures {
         compose = true
     }
