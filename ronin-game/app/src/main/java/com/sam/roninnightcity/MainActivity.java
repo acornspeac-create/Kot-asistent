618b4a6721eed721d0954a56f848c92d275a30f6
acornspeac-create <acornspeac@gmail.com>
2026-09-25 00:07:01 +0500

    Add RONIN Night City Android prototype

---
+++ ronin-game/app/src/main/java/com/sam/roninnightcity/MainActivity.java
@@ -0,0 +1,69 @@
+package com.sam.roninnightcity;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+  private WebView webView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    getWindow().getDecorView().setSystemUiVisibility(
+        View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+    );
+
+    webView = new WebView(this);
+    webView.setBackgroundColor(0xFF05070D);
+    WebView.setWebContentsDebuggingEnabled(false);
+    webView.setWebViewClient(new WebViewClient() {
+      private boolean allowed(Uri uri) {
+        return uri != null && "file".equals(uri.getScheme()) && "/android_asset/index.html".equals(uri.getPath());
+      }
+      @Override public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        return !allowed(request.getUrl());
+      }
+      @Override public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        return !allowed(Uri.parse(url));
+      }
+    });
+
+    WebSettings s = webView.getSettings();
+    s.setJavaScriptEnabled(true);
+    s.setDomStorageEnabled(true);
+    s.setAllowFileAccess(true);
+    s.setAllowContentAccess(false);
+    s.setMediaPlaybackRequiresUserGesture(false);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      s.setMixedContentMode(WebSettings.MIXED_CONTENT_NEVER_ALLOW);
+    }
+
+    setContentView(webView);
+    webView.loadUrl("file:///android_asset/index.html");
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    getWindow().getDecorView().setSystemUiVisibility(
+        View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+    );
+  }
+
+  @Override
+  protected void onDestroy() {
+    if (webView != null) webView.destroy();
+    super.onDestroy();
+  }
+}
